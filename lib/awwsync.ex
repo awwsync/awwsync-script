ae3e5eebf672e1d16f8f054ee167b9645c051992
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-12 23:58:56 +0400

    initial commit

---
+++ lib/awwsync.ex
@@ -0,0 +1,18 @@
+defmodule AWWSYNC do
+  @moduledoc """
+  Documentation for `AWWSYNC`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AWWSYNC.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/awwsync_test.exs
@@ -0,0 +1,8 @@
+defmodule AWWSYNCTest do
+  use ExUnit.Case
+  doctest AWWSYNC
+
+  test "greets the world" do
+    assert AWWSYNC.hello() == :world
+  end
+end
