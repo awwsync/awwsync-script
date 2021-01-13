004d19d1647c221df3032031757ff76e3d57f584
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-14 00:53:44 +0400

    add .envrc/tentacat conf

--- lib/github.ex
+++ lib/github.ex
@@ -1,7 +1,7 @@
 defmodule Github do
-  @client Tentacat.Client.new()
+  @client Tentacat.Client.new(%{access_token: System.get_env("GITHUB_ACCESS_TOKEN", "")})
 
   def get_events() do
-    Tentacat.Issues.Events.list_all(@client, "awwsync", "awwsync-script")
+    Tentacat.Issues.Events.list_all(@client, "facebook", "react")
   end
 end
