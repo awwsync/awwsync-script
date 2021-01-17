920e38c87213a34588f5b2c974eb3306795207c8
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-17 19:38:43 +0400

    wip stuff

--- lib/github.ex
+++ lib/github.ex
@@ -2,6 +2,12 @@ defmodule Github do
   @client Tentacat.Client.new(%{access_token: System.get_env("GITHUB_ACCESS_TOKEN", "")})
 
   def get_events() do
-    Tentacat.Issues.Events.list_all(@client, "facebook", "react")
+    {response, data, client} = Tentacat.Issues.Events.list_all(@client, "facebook", "react")
+
+    # parse_events(data)
+  end
+
+  def parse_events(events) do
+    events
   end
 end
